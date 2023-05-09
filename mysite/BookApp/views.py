911f7fa62e1d96331de7997c265b4d18337bfb95
Михаил Тарбеев <tarb3@yandex.ru>
2023-05-09 12:58:27 +0300

    Add Index(ListView), url

--- mysite/BookApp/views.py
+++ mysite/BookApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import ReadBooks
 
-# Create your views here.
+
+class Index(ListView):
+    model = ReadBooks
+    template_name = 'BookApp/index.html'
+    context_object_name = 'books'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
